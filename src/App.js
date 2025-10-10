deece5c43cd27f46a24be16ef1fa20fe22d8f8be
MD Tawsif Khan Siam <mdtawsifkhan.siam@georgebrown.ca>
2025-10-10 00:46:58 -0400

    Dep inclusions (#2)
    * added project dependencies to our package.json
    
    * adding our dependency lock file
    
    * including application source
    
    * adding all changes at once which includes package.json work and app src
    
    * pack
    
    * rebiuld
    
    * GitHub Action for github pages added
    
    * Name change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101515575 || MD Tawsif Khan Siam || DevOps</h5>
+      <h5>Developed By: 101515575 || MD Tawsif Khan Siam || COMP3104 ||DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
